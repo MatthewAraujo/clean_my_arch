94ae12fcb4df27653b1a3a7bf5f47cf8fefba57b
Matthew Araujo <matthewaraujo20@gmai.com>
2024-03-28 20:02:29 -0300

    initial commit: things are already doing

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clean package cache
+sudo yay -Scc
+# Remove unused packages (orphans)
+sudo yay -Rns $(yay -Qtdq)
+
+# Clean the cache in your /home directory
+rm -rf ~/.cache/*
+
+# Remove old config files
+rm -rf ~/.local/share/*
+
+# Remove duplicates, empty files, empty directories and broken symlinks
+rmlint /home/$(whoami) 
+./rmlint.sh
